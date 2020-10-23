49a7be1e6e6aeebcb57475c490d98396dfe4b6d8
stnamco <lbnamco15@gmail.com>
2020-10-23 21:38:52 +0900

    add web modulw

---
+++ Web/Web.h
@@ -0,0 +1,18 @@
+//
+//  Web.h
+//  Web
+//
+//  Created by kazuhiro nanko on 2020/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Web.
+FOUNDATION_EXPORT double WebVersionNumber;
+
+//! Project version string for Web.
+FOUNDATION_EXPORT const unsigned char WebVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Web/PublicHeader.h>
+
+
---
+++ Web/WebView.swift
@@ -0,0 +1,22 @@
+//
+//  WebView.swift
+//  Web
+//
+//  Created by kazuhiro nanko on 2020/10/23.
+//
+
+import SwiftUI
+
+public struct WebView: View {
+    public var body: some View {
+        Text("Hello, WebView!")
+    }
+    
+    public init() {}
+}
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView()
+    }
+}
