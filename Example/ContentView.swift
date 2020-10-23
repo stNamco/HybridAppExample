b83a8c1565d4be133771089423f2f71db23cbb84
stnamco <lbnamco15@gmail.com>
2020-10-23 21:30:09 +0900

    initiate project

---
+++ Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by kazuhiro nanko on 2020/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
