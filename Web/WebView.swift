bca5bcaaa47cadff87ea82acd38351c7a5e96483
stnamco <lbnamco15@gmail.com>
2020-10-24 08:57:01 +0900

    enable to show webview

--- Web/WebView.swift
+++ Web/WebView.swift
@@ -6,20 +6,30 @@
 //
 
 import SwiftUI
+import WebKit
 
-public struct WebView: View {
+public struct WebView: UIViewRepresentable {
+    public typealias UIViewType = WKWebView
+    public let urlString: String
+    
+    public func makeUIView(context: Context) -> WKWebView {
+        print(context)
+        return WKWebView()
+    }
+    
+    public func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(URLRequest(url: URL(string: urlString)!))
+    }
     
     @EnvironmentObject var viewModel: WebViewModel
     
-    public var body: some View {
-        Text("Hello, WebView!")
+    public init(urlString: String) {
+        self.urlString = urlString
     }
-    
-    public init() {}
 }
 
 struct WebView_Previews: PreviewProvider {
     static var previews: some View {
-        WebView()
+        WebView(urlString: "https://www.apple.com")
     }
 }
