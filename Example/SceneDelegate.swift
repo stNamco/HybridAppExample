a35ea05e0b3d9bd230d0638ca934f0463535a8ad
stnamco <lbnamco15@gmail.com>
2020-10-24 13:25:40 +0900

    remove unnecesary code

--- Example/SceneDelegate.swift
+++ Example/SceneDelegate.swift
@@ -20,11 +20,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        // Create the SwiftUI view that provides the window contents.
-//        let contentView = ContentView()
-//        let contentView = WebView(urlString: "https://www.apple.com")
-//        let contentView = QRCodeReaderView()
         
         let sceneRouter: GlobalSceneRouter = GlobalSceneRouter(
             webWirefrmae: Web.Router(),
