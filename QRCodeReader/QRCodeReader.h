65c9bd279a0c25ecde7e4c4fabb7f3651f8b00b3
stnamco <lbnamco15@gmail.com>
2020-10-24 11:48:37 +0900

    added qrcode reader

---
+++ QRCodeReader/QRCodeReader.h
@@ -0,0 +1,18 @@
+//
+//  QRCodeReader.h
+//  QRCodeReader
+//
+//  Created by kazuhiro nanko on 2020/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QRCodeReader.
+FOUNDATION_EXPORT double QRCodeReaderVersionNumber;
+
+//! Project version string for QRCodeReader.
+FOUNDATION_EXPORT const unsigned char QRCodeReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRCodeReader/PublicHeader.h>
+
+
