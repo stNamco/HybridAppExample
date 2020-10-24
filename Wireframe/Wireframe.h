94a8ef28cf3ee3a979cdc5570f3b9c97627f1784
stnamco <lbnamco15@gmail.com>
2020-10-24 11:47:48 +0900

    added wireframe

---
+++ Wireframe/Wireframe.h
@@ -0,0 +1,18 @@
+//
+//  Wireframe.h
+//  Wireframe
+//
+//  Created by kazuhiro nanko on 2020/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Wireframe.
+FOUNDATION_EXPORT double WireframeVersionNumber;
+
+//! Project version string for Wireframe.
+FOUNDATION_EXPORT const unsigned char WireframeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wireframe/PublicHeader.h>
+
+
